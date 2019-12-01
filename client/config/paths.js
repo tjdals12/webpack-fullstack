923a1db63be5a84c48dca99d83dc3b312aacdcd8
Seongmin <lll2slll@naver.com>
2019-12-01 20:26:24 +0900

    [2019. 12. 01] 1. Client 설정

---
+++ client/config/paths.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+const moduleFileExtensions = ["js", "jsx"];
+const resolveModule = (resolveFn, filePath) => {
+  const extension = moduleFileExtensions.find(ext =>
+    fs.existsSync(resolveFn(`${filePath}.${ext}`))
+  );
+
+  if (extension) {
+    return resolveFn(`${filePath}.${extension}`);
+  }
+
+  return resolveFn(`${filePath}.js`);
+};
+
+process.env.NODE_PATH = (process.env.NODE_PATH || "")
+  .split(path.delimiter)
+  .filter(folder => folder && !path.isAbsolute(folder))
+  .map(folder => path.resolve(appDirectory, folder))
+  .join(path.delimiter);
+
+module.exports = {
+  appHtml: resolveApp("public/index.html"),
+  appIndexJs: resolveModule(resolveApp, "src/index"),
+  appSrc: resolveApp("src")
+};
+
+module.exports.moduleFileExtensions = moduleFileExtensions;
---
+++ client/config/webpack.config.prod.js
@@ -0,0 +1,85 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HardSourceWebpackPlugin = require("hard-source-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const paths = require("./paths");
+
+module.exports = {
+  name: "for production",
+  mode: "production",
+  devtool: "cheap-module-souce-map",
+  resolve: {
+    extensions: paths.moduleFileExtensions.map(ext => `.${ext}`),
+    modules: ["node_modules"].concat(
+      process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
+    )
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: paths.appHtml }),
+    new MiniCssExtractPlugin({ filename: "style.css" }),
+    new CleanWebpackPlugin(),
+    new HardSourceWebpackPlugin(),
+    new BundleAnalyzerPlugin()
+  ],
+  entry: {
+    app: ["@babel/polyfill", paths.appIndexJs]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+              plugins: [
+                "@babel/plugin-proposal-class-properties",
+                "@babel/plugin-proposal-object-rest-spread"
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {}
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.(scss|sass)$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "statics"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    path: path.join(__dirname, "../dist"),
+    filename: "[name].js"
+  }
+};
